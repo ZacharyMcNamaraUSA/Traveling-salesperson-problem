093d039a5bf8178c64553c8d9c12fbf939137542
Zachary McNamara <zachary.mcnamara.github@gmail.com>
2020-12-14 13:49:44 -0600

    Added datetime print
    Truck object has the function to designate if a package is loaded, delayed, or delivered by a user-specified datetime

--- truck.py
+++ truck.py
@@ -1,20 +1,32 @@
 # t_graph.py by Zachary McNamara zmcnama@my.wgu.edu ID#001182706
 
+from collections import deque
+import datetime
+import time
+
 
 class Truck:
     def __init__(self, label):
         self.label = label
-        self.loaded_package_keys = []
+        self.packages_list = []
+        self.delivery_order = deque
+        self.speed = 18
         self.miles_driven = 0.0
-        self.delivered_package_keys = []
+        self.delivered_packages = []
+        self.time_tracker = datetime.datetime(2000, 1, 1, 8, 0, 0)
+        self.location_address = "4001 South 700 East"
 
     @property
-    def loaded_package_keys(self):
-        return self._loaded_package_keys
+    def packages_list(self):
+        return self._packages_list
 
-    @loaded_package_keys.setter
-    def loaded_package_keys(self, keys):
-        self._loaded_package_keys = keys
+    @packages_list.setter
+    def packages_list(self, packs):
+        self._packages_list = packs
+
+    @packages_list.deleter
+    def packages_list(self):
+        del self._packages_list
 
     @property
     def miles_driven(self):
@@ -24,27 +36,48 @@ class Truck:
     def miles_driven(self, miles):
         self._miles_driven = miles
 
+    @property
+    def delivery_order(self):
+        return self._delivery_order
 
-class Graph:
-    def __init__(self, vert_list=[]):
-        self.adjacency_list = {}
-        self.edge_weights = {}
+    @delivery_order.setter
+    def delivery_order(self, value):
+        self._delivery_order = value
 
-    def add_vertex(self, new_vertex):
-        self.adjacency_list[new_vertex] = []
+    @delivery_order.deleter
+    def delivery_order(self):
+        del self._delivery_order
 
-    def add_directed_edge(self, from_vertex, to_vertex, weight=1.0):
-        self.edge_weights[(from_vertex, to_vertex)] = weight
-        self.adjacency_list[from_vertex].append(to_vertex)
-        # print("adding an edge between " + from_vertex.label +
-        #       " to " + to_vertex.label + " of distance " + str(weight))
+    def load_package(self, package):
+        max_index = len(package) - 1
+        package[max_index] = "Loaded on " + self.label
+        self._packages_list.append(package)
 
-    def add_undirected_edge(self, vertex_a, vertex_b, weight=1.0):
-        self.add_directed_edge(vertex_a, vertex_b, weight)
-        self.add_directed_edge(vertex_b, vertex_a, weight)
+    def deliver_package(self, package, miles):
+        self.delivered_packages.append(package)
+        self.location_address = package[1]
+        self._miles_driven += miles
 
-    def set_adjacent_vertexes(self, current_vertex, adjacents=[]):
-        self.adjacency_list[current_vertex] = adjacents
+    def print_packages_status(self, specified_datetime):
+        for p in self.packages_list:
+            time_string = p[8][-11:-3]
+            delivered_time = datetime.datetime.strptime(time_string, "%H:%M:%S")
+
+            # if the package has not been 'delivered' yet, it is still loaded.
+            if delivered_time > specified_datetime:
+                t_package = list(p)
+                t_package[8] = "Loaded in " + self.label
+
+                if "Delayed" in p[7]:
+                    t_package = list(p)
+                    t_package[8] = "Delay shipping. Assign to " + self.label
+            else:
+                t_package = list(p)
+                t_package[8] = "Delivered by {} at ".format(self.label) + time_string
+
+            print("\t\t{}".format(t_package))
+
+
 
 
 
